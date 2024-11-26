f905391b8e6ed2774a39194b8f1989786dd7094b
hidelrios <hidels.rios@gmail.com>
2024-11-25 21:39:34 -0300

    feat: cria config env

--- db/migrations/20241125231015_create-user.ts
+++ db/migrations/20241125231015_create-user.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
         table.increments('id').primary();
         table.string('username').notNullable().unique();
         table.string('password').notNullable();
-        table.string('email').notNullable().unique();
+        table.string('email').notNullable().unique(); 
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
@@ -10,7 +11,7 @@ app.get("/ping", async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("Http server running !!! 🚀");
