ac8c455330ce24a74f57de5d891baf4495c76f29
hidelrios <hidels.rios@gmail.com>
2024-11-30 11:13:10 -0300

    feat: adiciona metricas de refeicoes do usuario

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -162,4 +162,49 @@ export async function mealsRoutes(app: FastifyInstance) {
       return res.status(200).send({ message: "Refeição deletada" });
     }
   );
+  // Obtem metricas do usuario
+  app.get(
+    "/metrics",
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+        
+      const totalMeals = await knex("meals")
+        .where({ user_id: request.user?.id })
+        .orderBy("date", "desc");
+
+      const totalMealsOnDiet = await knex("meals")
+        .where({ user_id: request.user?.id, is_on_diet: true })
+        .count("id", { as: "total" })
+        .first();
+
+      const totalMealsOffDiet = await knex("meals")
+        .where({ user_id: request.user?.id, is_on_diet: false })
+        .count("id", { as: "total" })
+        .first();
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1;
+          } else {
+            acc.currentSequence = 0;
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence;
+          }
+
+          return acc;
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 }
+      );
+
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      });
+    }
+  );
 }
